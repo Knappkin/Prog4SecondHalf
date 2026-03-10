a49d9b3bfd76d9f851576bbb2420e25bb3c629c8
Knappkin <44062671+Knappkin@users.noreply.github.com>
2026-03-10 18:49:58 -0400

    Some failed attempts at getting things working

--- Assets/Scripts/Tasks/SwitchDirectionAT.cs
+++ Assets/Scripts/Tasks/SwitchDirectionAT.cs
@@ -11,21 +11,23 @@ namespace NodeCanvas.Tasks.Actions {
 		public BBParameter<Transform> pointB;
 		public BBParameter<Transform> targetPosition;
 
-
-		//This is called once each time the task is enabled.
-		//Call EndAction() to mark the action as finished, either in success or failure.
-		//EndAction can be called from anywhere.
-		protected override void OnExecute() {
+      
+        //This is called once each time the task is enabled.
+        //Call EndAction() to mark the action as finished, either in success or failure.
+        //EndAction can be called from anywhere.
+        protected override void OnExecute() {
 
 			if (targetPosition == pointA)
 			{
 				targetPosition = pointB;
-			}
+                EndAction();
+            }
 			else
 			{
 				targetPosition = pointA;
-			}
-				EndAction(true);
+                EndAction();
+            }
+				
 		}
 
 		
