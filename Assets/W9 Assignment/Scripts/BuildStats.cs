569f62e14e844b06070818c464bef6c220b3450a
Knappkin <44062671+Knappkin@users.noreply.github.com>
2026-03-18 21:49:02 -0400

    Worked on W9 Assignment

---
+++ Assets/W9 Assignment/Scripts/BuildStats.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildStats", menuName = "Scriptable Objects/BuildStats")]
+public class BuildStats : ScriptableObject
+{
+
+
+    public float buildWidth;
+    public float buildHeight;
+    public float buildTime;
+
+}
